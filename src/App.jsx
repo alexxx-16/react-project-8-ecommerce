371d1c34f7d507131400ea3e69768ca3cb6602d5
Alex Ge-Hall <xan-y@outlook.com>
2026-03-09 17:44:04 +1000

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import SignUpForm from "./components/SignUpForm";
 import "./index.css";
+import { Route, Routes } from "react-router-dom";
+
+function HomePage() {
+  return (
+    <div className="flex flex-col w-full min-h-screen items-center justify-center bg-pink-200">
+      <h1 className="text-4xl">Home Page</h1>
+    </div>
+  );
+}
+function AboutPage() {
+  return (
+    <div className="flex flex-col w-full min-h-screen items-center justify-center bg-pink-200">
+      <h1 className="text-4xl">About Page</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="flex flex-col w-full min-h-screen items-center justify-center bg-pink-200">
-      <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
   );
 }
 
