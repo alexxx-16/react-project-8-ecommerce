e012fd218e81c2bf6f1878f9626ce20febc6c409
Alex Ge-Hall <xan-y@outlook.com>
2026-03-16 04:18:57 +1000

    save progress

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,17 @@ function HomePage() {
     </div>
   );
 }
-function AboutPage() {
+function ProfilePage() {
   return (
     <div className="flex flex-col w-full min-h-screen items-center justify-center bg-pink-200">
-      <h1 className="text-4xl">About Page</h1>
+      <h1 className="text-4xl">Profile Page</h1>
+    </div>
+  );
+}
+function LoginPage() {
+  return (
+    <div className="flex flex-col w-full min-h-screen items-center justify-center bg-pink-200">
+      <h1 className="text-4xl">Login Page</h1>
     </div>
   );
 }
@@ -20,13 +27,15 @@ function AboutPage() {
 function App() {
   return (
     <div>
-      <nav className="flex gap-4 mx-4 my-2">
+      <nav className="flex gap-4 px-4 py-2 bg-pink-300">
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/login">Login</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route
           path="*"
           element={<h1 className="text-4xl text-center">404 Not Found</h1>}
